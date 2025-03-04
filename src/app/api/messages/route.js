ecc2d1f6e7e4bf42bcd27d7a50ce98ac5b4b4a8f
abdo_samii <168330414+Abdo-samii@users.noreply.github.com>
2025-03-04 14:49:04 +0200

    Update route.js

--- src/app/api/messages/route.js
+++ src/app/api/messages/route.js
@@ -6,8 +6,6 @@ export async function POST(req) {
       return Response.json({ error: "All fields are required" }, { status: 400 });
     }
 
-    // قم بإرسال البريد هنا
-
     return Response.json({ success: true, message: "Email sent successfully!" }, { status: 200 });
   } catch (error) {
     return Response.json({ error: "Internal Server Error" }, { status: 500 });
