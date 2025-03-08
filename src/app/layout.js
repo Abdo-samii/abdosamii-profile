4de26de99d7ef10829be8c3db2eda6de87c92d6b
abdo_samii <abdosamii2003@gmail.com>
2025-03-08 17:13:22 +0200

    Fix deployment issues

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,17 @@
-"use client"
-import './globals.css'
-import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
+import './globals.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
 export const metadata = {
   title: 'abdo_samii',
   description: 'abdo_samii is a web developer, mobile developer, and UI/UX designer.',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
